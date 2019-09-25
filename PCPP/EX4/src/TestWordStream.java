c88f5b55d787379c00c722edde0fe19ecf549251
Alex Ardelean <ardeleanalexandru3@gmail.com>
2019-09-25 16:36:38 +0300

    Update TestWordStream.java

--- PCPP/EX4/src/TestWordStream.java
+++ PCPP/EX4/src/TestWordStream.java
@@ -9,30 +9,70 @@ import java.util.*;
 import java.util.Map.Entry;
 import java.util.function.DoubleSupplier;
 import java.util.function.IntSupplier;
-import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.stream.*;
 
 public class TestWordStream {
   public static void main(String[] args) {
-    String filename = "/usr/share/dict/words";
+    //String filename = "/usr/share/dict/words";
+    String filename = "words";
+    // 4.4.1
+    System.out.println("4.4.1");
     System.out.println(readWords(filename).count());
+    // 4.4.2
+    System.out.println("4.4.2");
+    readWords(filename).limit(100).forEach(System.out::println);
+    // 4.4.3
+    System.out.println("4.4.3");
+    readWords(filename).filter((x) -> x.length() >= 22).forEach(System.out::println);
+    // 4.4.4
+    System.out.println("4.4.4");
+    readWords(filename).filter((x) -> x.length() >= 22).findAny().ifPresent(System.out::println);
+    // 4.4.5
+    System.out.println("4.4.5");
+    readWords(filename).filter(TestWordStream::isPalindrome).forEach(System.out::println);
+    // 4.4.6
+    System.out.println("4.4.6");
+    readWords(filename).parallel().filter(TestWordStream::isPalindrome).forEach(System.out::println);
+    // 4.4.7
+    System.out.println("4.4.7");
+    IntSummaryStatistics s = readWords(filename).mapToInt((x) -> (int)x.chars().count()).summaryStatistics();
+    System.out.println("Min: " + s.getMin());
+    System.out.println("Max: " + s.getMax());
+    System.out.println("Avg: " + s.getAverage());
+    // 4.4.8
+    System.out.println("4.4.8");
+    readWords(filename);
+    // 4.4.9
+    System.out.println("4.4.9");
+    readWords(filename);
+    // 4.4.10
+    System.out.println("4.4.10");
+    readWords(filename);
+    // 4.4.11
+    System.out.println("4.4.11");
+    readWords(filename);
+    // 4.4.12
+    System.out.println("4.4.12");
+    readWords(filename);
+    // 4.4.13
+    System.out.println("4.4.13");
+    readWords(filename);
 
-    // *************
+    // *********************************
     // DoubleStreams
+    int range = 1_000_000_000;
     // 4.5.1
-    final DoubleStream doubleStream1 = IntStream.range(1, 1_000_000_000).mapToDouble((x) -> 1.0 / x / x);
+    final DoubleStream doubleStream1 = IntStream.range(1, range).mapToDouble((x) -> 1.0 / x / x);
     final double sum1 = doubleStream1.sum();
     System.out.printf("Sum = %20.16f%n", sum1 - Math.PI * Math.PI / 6);
 
     // 4.5.2
-    final DoubleStream doubleStream2 = IntStream.range(1, 1_000_000_000).parallel().mapToDouble((x) -> 1.0 / x / x);
+    final DoubleStream doubleStream2 = IntStream.range(1, range).parallel().mapToDouble((x) -> 1.0 / x / x);
     final double sum2 = doubleStream2.parallel().sum();
     System.out.printf("Sum = %20.16f%n", sum2 - Math.PI * Math.PI / 6);
 
     // 4.5.3
-    final DoubleStream doubleStream3 = IntStream.range(1, 1_000_000_000).parallel().mapToDouble((x) -> 1.0 / x / x);
+    final DoubleStream doubleStream3 = IntStream.range(1, range).parallel().mapToDouble((x) -> 1.0 / x / x);
     final double[] doublesList = doubleStream3.toArray();
     double sum3 = 0;
     for (double aDoublesList : doublesList) {
@@ -51,12 +91,12 @@ public class TestWordStream {
       }
     };
 
-    final DoubleStream doubleStream4 = DoubleStream.generate(doubleSupplier).limit(1_000_000_000);
+    final DoubleStream doubleStream4 = DoubleStream.generate(doubleSupplier).limit(range);
     final double sum4 = doubleStream4.sum();
     System.out.printf("Sum = %20.16f%n", sum4 - Math.PI * Math.PI / 6);
 
     // 4.5.5
-    final DoubleStream doubleStream5 = DoubleStream.generate(doubleSupplier).limit(1_000_000_000);
+    final DoubleStream doubleStream5 = DoubleStream.generate(doubleSupplier).limit(range);
     final double sum5 = doubleStream5.parallel().sum();
     System.out.printf("Sum = %20.16f%n", sum5 - Math.PI * Math.PI / 6);
   }
@@ -64,16 +104,18 @@ public class TestWordStream {
   public static Stream<String> readWords(String filename) {
     try {
       BufferedReader reader = new BufferedReader(new FileReader(filename));
-      // TO DO: Implement properly
-      return Stream.<String>empty(); 
+      return reader.lines();
     } catch (IOException exn) { 
       return Stream.<String>empty();
     }
   }
 
   public static boolean isPalindrome(String s) {
-    // TO DO: Implement properly
-    return false; 
+    Iterator<Integer> i1 = s.chars().boxed().collect(Collectors.toCollection(ArrayDeque::new)).descendingIterator();
+    Iterator<Integer> i2 = s.chars().boxed().iterator();
+    while(i1.hasNext() && i2.hasNext())
+      if (!i1.next().equals(i2.next())) return false;
+    return true;
   }
 
   public static Map<Character,Integer> letters(String s) {
